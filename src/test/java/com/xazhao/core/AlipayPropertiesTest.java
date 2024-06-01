158e3501f9c2f0ac3718a151647d1bf70e1e622a
xazhao <xazhao@126.com>
2024-06-01 23:08:26 +0800

    Gitee同步GitHub

--- src/test/java/com/xazhao/core/AlipayPropertiesTest.java
+++ src/test/java/com/xazhao/core/AlipayPropertiesTest.java
@@ -1,5 +1,7 @@
 package com.xazhao.core;
 
+import cn.hutool.core.date.DateUtil;
+import com.xazhao.constant.Constant;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +10,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @Description Created on 2024/05/11.
@@ -27,5 +32,27 @@ public class AlipayPropertiesTest {
     public void getAlipayPropertiesTest() {
 
         log.info(properties.getProperty("merchant-private-key"));
+    }
+
+    @Test
+    public void getOffsetDay() {
+
+        String format = DateUtil.format(DateUtil.offsetDay(new Date(), 180), Constant.NORM_DATETIME_PATTERN);
+        log.info(format);
+
+        List<String> insurance = new ArrayList<>();
+        insurance.add("V001");
+        insurance.add("V002");
+        insurance.add("V003");
+        insurance.add("V004");
+        insurance.add("V016");
+        insurance.add("V006");
+        insurance.add("V012");
+        insurance.add("V008");
+        List<Integer> versionList = new ArrayList<>();
+        insurance.forEach(caSubInsurance ->
+                versionList.add(Integer.parseInt(caSubInsurance.split("V")[0])));
+
+        log.info(versionList.toString());
     }
 }
